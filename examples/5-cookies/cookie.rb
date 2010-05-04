7b26cbb1dc6facfa9a08f13a8b4453f609953e9f
Mario Aquino <mario.e.aquino@gmail.com>
2010-05-04 06:22:09 -0500

    Improved cookies examples

--- examples/5-cookies/cookie.rb
+++ examples/5-cookies/cookie.rb
@@ -5,5 +5,5 @@ enable :sessions
 
 get "/" do
   session["counter"] ||= 0
-  "Hello, World! #{session["counter"] = session["counter"] + 1}"
+  "Number of visits: #{session["counter"] = session["counter"] + 1}"
 end
--- examples/6-advanced_cookies/advanced_cookies.rb
+++ examples/6-advanced_cookies/advanced_cookies.rb
@@ -3,7 +3,8 @@ require 'sinatra'
 
 get "/" do
   counter = request.cookies['counter'].to_i || 0
-  expiration = Time.now + (60 * 2)
-  response.set_cookie('counter', :value => (counter += 1), :expires => expiration)
-  "Hello, World! #{counter}"
+  seconds_in_the_future = 30
+  expiration = Time.now + seconds_in_the_future
+  response.set_cookie('counter', :value => counter += 1, :expires => expiration)
+  "You've been here #{counter} times and the cookies expires: #{expiration}"
 end
